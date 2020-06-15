574612cf8931406995e642b4ec39e62b6ccd704b
jaycer95 <jacer.dabbabi95@gmail.com>
2020-06-15 11:13:03 +0000

    afzf

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,34 +3,34 @@
  * bubble_sort - sorts an array of integers in ascending order
  * @array: array to be sorted
  * @size: size of the array
- * @Return: void
+ * Return: void
  **/
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i;
 	bool condition;
 
-	while(condition == true)
+	if (size < 2)
+		return;
+	while (condition == true)
 	{
 		condition = false;
 		for (i = 0; i < size - 1; i++)
 		{
-			if ( array[i] > array[i + 1])
+			if (array[i] > array[i + 1])
 			{
 				swapping(array, i);
 				print_array(array, size);
 				condition = true;
 			}
-			/*print_array(array, size);*/
 		}
 	}
 }
 /**
- *
- *
- *
- *
- *
+ * swapping - swaps integers
+ * @array: array
+ * @i: int
+ * Return: void
  **/
 void swapping(int *array, int i)
 {
